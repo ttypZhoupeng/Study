4ece13367746d3977dc8694c5498c94c1ac21d8c
zhoupeng <2460155936@qq.com>
2017-07-28 00:59:09 +0800

    7.28 凌晨

--- 20170727_js第九天/task/侧边导航/js/main.js
+++ 20170727_js第九天/task/侧边导航/js/main.js
@@ -1,11 +1,18 @@
 var oLi = document.getElementsByTagName("li");
+var oContent = document.getElementsByClassName("content");
+
 
 for(var i = 0;i<oLi.length;i++){
-    oLi[i].onclick = function(){
-        for(var j=0;j<oLi.length;j++){
-            oLi[j].className = "";
+    (function(m){
+        oLi[m].onclick = function(){
+            for(var j=0;j<oLi.length;j++){
+                oLi[j].className = "";
+                oContent[j].style.display = "none";
+            }
+            this.className = "current";
+            oContent[m].style.display = "block";
         }
-        this.className = "current";
-    }
+        
+    })(i);
     
 }
