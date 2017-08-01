ea30b07b761ca3fb613f8631994572ea26b1bb0c
zhoupeng <2460155936@qq.com>
2017-08-02 00:31:32 +0800

    08/02 凌晨

--- 20170801_js正则/task/js/main.js
+++ 20170801_js正则/task/js/main.js
@@ -4,7 +4,11 @@ var oCpw = document.getElementById("confirm_password");
 var oTel = document.getElementById("tel");
 var oEmail = document.getElementById("email");
 var oBtn = document.getElementById("btn");
-
+var oIdc = document.getElementById("id_card");
+var oCt = document.getElementById("Chine_tel");
+var otel2 = document.getElementById("tel2");
+var oUrl = document.getElementById("url");
+var oBirth = document.getElementById("birthday");
 
 
 console.log(oName);
@@ -48,5 +52,44 @@ oBtn.onclick = function(){
         alert("请输入邮箱");
     }else if(!re4.test(oEmail.value)){
         alert("该邮箱不存在");
+    }
+
+    var re5 = /^\d{15}|\d{18}$/;
+    if(oIdc.value==""){
+        alert("请输入身份证号");
+    }else if(!re5.test(oIdc.value)){
+        alert("该身份证号不存在");
+    }
+
+    var re6 = /\d{3}-\d{8}|\d{4}-\d{7}/;
+    if(oCt.value==""){
+        alert("请输入国内电话号码");
+    }else if(!re6.test(oCt.value)){
+        alert("该国内电话号码不存在");
+    }
+
+
+    var re7 = /^($$\d{3,4}-)|\d{3.4}-)?\d{7,8}$/;
+    if(otel2.value==""){
+        alert("请输入电话号码");
+    }else if(!re7.test(otel2.value)){
+        alert("该电话号码不存在");
+    }
+
+    
+
+    var re8 = /[a-zA-z]+:\/\/[^\s]*/;
+    if(oUrl.value==""){
+        alert("请输入InternetUrl");
+    }else if(!re8.test(oUrl.value)){
+        alert("该InternetUrl不存在");
+    }
+
+
+    var re9 = /^\d{4}-\d{1,2}-\d{1,2}/;
+    if(oBirth.value==""){
+        alert("请输入生日");
+    }else if(!re9.test(oBirth.value)){
+        alert("该生日格式不对，例如1999-10-10");
     }
 }
