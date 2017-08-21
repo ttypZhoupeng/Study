babcc4a694d252f84021c2526195da8fd16ba17b
zhoupeng <2460155936@qq.com>
2017-08-22 00:17:13 +0800

    0822 凌晨

--- ajax_php/WWW/pc_shop/js/main.js
+++ ajax_php/WWW/pc_shop/js/main.js
@@ -13,40 +13,6 @@ $(function(){
     })
 
 
-    // $.get("http://h6.duchengjiu.top/shop/api_goods.php",function(response){
-    //     var obj = response;
-        
-    //     for(var i=0;i<obj.data.length;i++){
-    //         $("#goodsUl").append('<li><img src="' +obj.data[i].goods_thumb+ '" /><p class="title"><a href="detail.html?goods_id='+obj.data[i].goods_id+'">' +obj.data[i].goods_name+ '</a></p><p class="desc">' +obj.data[i].goods_desc+ '</p><p class="price">￥' +obj.data[i].price+ '</p></li>');
-    //     }
-    // })
-
-
-
-    // 商品数据导入
-    $.get("http://h6.duchengjiu.top/shop/api_goods.php",function(response){
-        var obj = response;
-        
-        for(var i=0;i<obj.data.length;i++){
-            $("#goodsUl").append(
-            '<div class="hot_item"><img src="'
-            +obj.data[i].goods_thumb+
-            '"><div class="hot_item_cover"><p class="goods_price">'
-            +response.data[i].price+
-            '</p><div class="goods_content"><p class="goods_name">'
-            +response.data[i].goods_name+
-            '</p><p class="goods_desc">'
-            +response.data[i].goods_desc+
-            '</p></div></div><div class="bottom_bar"><img src="'
-            +obj.data[i].goods_thumb+
-            '"><span>'
-            +obj.data[i].goods_name+
-            '</span><div class="thumb_up"><span>'
-            +parseInt(obj.data[i].price)+
-            '</span><img src="images/heart.png"></div></div></div>' );
-        }
-    })
-
 
     // 轮播图
     var $banner = $("#banner");
