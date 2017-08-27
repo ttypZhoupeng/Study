b210ef8a9dd0a5840b84af72f607bcf9b247b35b
zhoupeng <2460155936@qq.com>
2017-08-27 23:58:01 +0800

    良仓基本完成

--- ajax_php/WWW/pc_shop/js/main.js
+++ ajax_php/WWW/pc_shop/js/main.js
@@ -1,3 +1,8 @@
+if(localStorage.getItem("token")){
+    console.log(222);
+    $(".login_register").html('<span><a href="#">'+localStorage.getItem("username")+'</a></span>');
+}
+
 $(function(){
 
 
@@ -78,4 +83,25 @@ $(function(){
 
         $focus.eq(n).addClass("cur").siblings().removeClass("cur");
     }
+
+    //搜索功能
+    $(".search_btn").click(function(){
+        var search = $("#search").val();
+        console.log(search);
+        $.ajax({
+            "url":"http://h6.duchengjiu.top/shop/api_goods.php",
+            "type":"get",
+            "dataType":"json",
+            "data":{
+                "search_text":search,
+            },
+            "success":function(response){
+                console.log(search);
+                // console.log(response);
+                var goodsId = response.data[0].goods_id;
+                console.log(goodsId);
+                window.location.href = "detail.html?goods_id="+goodsId;
+            }
+        })
+    })
 })
