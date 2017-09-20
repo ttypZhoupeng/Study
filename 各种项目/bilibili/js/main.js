34b4af28a48c03db806472ede06c2840c3c864bd
zhoupeng <2460155936@qq.com>
2017-09-21 00:01:53 +0800

    bilibili 凌晨

---
+++ 各种项目/bilibili/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#downBtn").click(function(){
+        $(".down-nav").slideDown(300);
+    })
+    $("#upBtn").click(function(){
+        $(".down-nav").slideUp(300);
+    })
+})
